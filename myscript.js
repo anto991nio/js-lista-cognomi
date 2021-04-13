7c9110f0456565d351dd84bb191671ea45a50402
anto991nio <antonio.91.cardone@gmail.com>
2021-04-13 15:56:11 +0200

    alcune correzioni

--- myscript.js
+++ myscript.js
@@ -7,18 +7,24 @@ var listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 var mioCognome = prompt("Inserisci il tuo cognome");
 
 
+
 /* inserisco il cognome nell'array */
 listaCognomi.push(mioCognome);
+/* var cognomeGrande ;
 
 /* rendo tutti i cognomi in maiuscolo */
-listaCognomi = listaCognomi.map(function (x) { return x.toUpperCase(); })
+/* for( i = 0 ; i < listaCognomi.length ; i++ ) {
+    var cognomeGrande = listaCognomi[i];
+    console.log(cognomeGrande.toUpperCase());
+
+} */ 
 
 /* li stampo in ordine alfabetico */
 console.log(listaCognomi.sort());
 /* stampo la posizione del cognome inserito */
-console.log(listaCognomi.indexOf(mioCognome.toUpperCase()) + 1);
+console.log(listaCognomi.indexOf(mioCognome)+1);
 
-document.getElementById("miocognome").innerHTML = "Il tuo cognome è:" + " " + mioCognome.toUpperCase();
+document.getElementById("miocognome").innerHTML = "Il tuo cognome è:" + " " + mioCognome;
 document.getElementById("listcongnomi").innerHTML = "La lista cognomi in ordine alfabetico è la seguente:" + " " + listaCognomi;
-document.getElementById("tuoindice").innerHTML = "Il tuo cognome è in posizione :" + " " + (listaCognomi.indexOf(mioCognome.toUpperCase()) + 1);
+document.getElementById("tuoindice").innerHTML = "Il tuo cognome è in posizione :" + " " + (listaCognomi.indexOf(mioCognome)+1);
 
